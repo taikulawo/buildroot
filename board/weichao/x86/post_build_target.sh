42da7544b6e724db3bd8d5ce6b8f06b7c10a0d8a
wuweichao.1 <wuweichao.1@bytedance.com>
2023-07-24 12:05:48 +0800

    add post-build.sh
    1
    
    test file dd
    
    1
    
    add .config
    
    1
    
    kernel增加curl
    
    openssh
    
    1
    
    1
    
    1
    
    BR2_ROOTFS_OVERLAY
    
    1
    
    1
    
    done 9p
    
    将配置都迁移到board/weichao/x86
    
    buildroot依赖外部linux repo (#1)
    
    * buildroot use custom kernel src path
    
    * python open r+ for both read and writing
    
    * 1
    
    * 1
    
    * buildroot的.config配置移动到board/并使用make savedefconfig管理，所以不再需要.config && .config.old
    
    * 1
    
    * xdp prog need libelf
    
    * buildroot使用的kernel .config绑定../linux/.config。
    
    今后只需要linux/.config同步到linux/arch/x86/configs/weichao_x86_64_defconfig
    
    * 11
    
    * 1
    
    * update readme
    
    ---------
    
    Co-authored-by: wuweichao.1 <wuweichao.1@bytedance.com>

---
+++ board/weichao/x86/post_build_target.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# 注释掉，每次修改文件make -j2后脚本都会运行
+# 给etc/写入重复的配置
+# 现在改用 BR2_ROOTFS_OVERLAY
+# https://stackoverflow.com/questions/17885451/how-to-rebuild-rootfs-in-buildroot
+
+# run after create rootfs
+# 此脚本在全部package(build/目录下，第三方依赖，kernel image)编译, initramfs(target/)构建完成，
+# 即将在images/生成最后的rootfs，kernel image bootloader 之前运行
+ROOT=${TARGET_DIR}/root
+OUTPUT_LOCATION=$BUILD_DIR
+ETC=${TARGET_DIR}/etc
+
+python3 ./board/weichao/x86/setup.py
---
+++ board/weichao/x86/setup.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+import os
+target_dir = os.environ["TARGET_DIR"]
+fs_etc = f'{target_dir}/etc'
+mnt_guest_path = f'{target_dir}/mnt/guest'
+
+os.system(f'mkdir -p {mnt_guest_path}')
+
+# mount guest 9plan fs
+fstab = f'{fs_etc}/fstab'
+with open(fstab, "r+",encoding="utf-8") as fstab_handle:
+    c = fstab_handle.read()
+    c1 = "guestroot   /mnt/guest    9p      trans=virtio,version=9p2000.L   0 0"
+    if c1 not in c:
+        fstab_handle.write(f"\n{c1}\n")
