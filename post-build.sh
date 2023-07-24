d7b75b7be8340b82997fa263adc4c1d2f5b2200f
wuweichao.1 <wuweichao.1@bytedance.com>
2023-07-24 13:45:12 +0800

    test file dd

--- post-build.sh
+++ post-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # run after create rootfs
+ROOT=${TARGET_DIR}/root
 touch ${TARGET_DIR}/root/hello.txt
-echo world > ${TARGET_DIR}/root/hello.txt
+# test large file
+dd if=/dev/zero of=$ROOT/rootfs.ext4 bs=1M count=100
+echo world > $ROOT/hello.txt
