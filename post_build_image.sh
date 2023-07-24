ba5e21d066d63c4f1bf88b24cb3e74e26d15d8f4
wuweichao.1 <wuweichao.1@bytedance.com>
2023-07-24 18:14:33 +0800

    1

--- post_build_image.sh
+++ post_build_image.sh
@@ -4,7 +4,17 @@
 # 即将在images/生成最后的rootfs，kernel image bootloader 之前运行
 ROOT=${TARGET_DIR}/root
 OUTPUT_LOCATION=$BUILD_DIR
+ETC=${TARGET_DIR}/etc
 echo $OUTPUT_LOCATION
+
 echo $TARGET_DIR
 # test large file
+echo $OUTPUT_LOCATION
+printf "PermitRootLogin yes\n\
+PermitEmptyPasswords" >> $ETC/ssh/sshd_config
 # dd if=/dev/zero of=$ROOT/rootfs.ext4 bs=1M count=100
+
+cat << EOF >> $ETC/network/interfaces
+auto eth0
+iface eth0 inet dhcp
+EOF
