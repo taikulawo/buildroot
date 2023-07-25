60cf316c46fd0378f1e9c704ecde627933427ad4
wuweichao.1 <wuweichao.1@bytedance.com>
2023-07-25 16:01:45 +0800

    python open r+ for both read and writing

--- board/weichao/x86/setup.py
+++ board/weichao/x86/setup.py
@@ -7,9 +7,10 @@ mnt_guest_path = f'{target_dir}/mnt/guest'
 os.system(f'mkdir -p {mnt_guest_path}')
 
 # mount guest 9plan fs
-fs_tab = f'{fs_etc}/fstab'
-handle = open(fs_tab, "a+")
-c = handle.read()
-c1 = "guestroot   /mnt/guest    9p      trans=virtio,version=9p2000.L   0 0\n"
-if c1 not in c:
-    handle.write(c1)
+fstab = f'{fs_etc}/fstab'
+with open(fstab, "r+",encoding="utf-8") as fstab_handle:
+    c = fstab_handle.read()
+    print("fstab:", fstab, c)
+    c1 = "guestroot   /mnt/guest    9p      trans=virtio,version=9p2000.L   0 0"
+    if c1 not in c:
+        fstab_handle.write(f"\n{c1}\n")
