2270aa7ceda6453f7af980a2ba0120ce73e17b30
wuweichao.1 <wuweichao.1@bytedance.com>
2023-07-25 11:06:22 +0800

    done 9p

--- post_build_target.sh
+++ post_build_target.sh
@@ -7,9 +7,9 @@
 # run after create rootfs
 # 此脚本在全部package(build/目录下，第三方依赖，kernel image)编译, initramfs(target/)构建完成，
 # 即将在images/生成最后的rootfs，kernel image bootloader 之前运行
-# ROOT=${TARGET_DIR}/root
-# OUTPUT_LOCATION=$BUILD_DIR
-# ETC=${TARGET_DIR}/etc
+ROOT=${TARGET_DIR}/root
+OUTPUT_LOCATION=$BUILD_DIR
+ETC=${TARGET_DIR}/etc
 # echo $OUTPUT_LOCATION
 
 # echo $TARGET_DIR
@@ -23,3 +23,5 @@
 # auto eth0
 # iface eth0 inet dhcp
 # EOF
+mkdir -p ${TARGET_DIR}/mnt/guest
+printf "guestroot   /mnt/guest    9p      trans=virtio,version=9p2000.L   0 0\n" >> $ETC/fstab
