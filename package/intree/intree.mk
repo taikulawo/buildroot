e83f15e3088b5abe037f02d16bbcc477e3d171fd
iamwwc <qaq1362211689@gmail.com>
2023-11-06 19:08:43 +0800

    1

--- package/intree/intree.mk
+++ package/intree/intree.mk
@@ -5,23 +5,24 @@
 ################################################################################
 
 INTREE_VERSION = 1.0
-INTREE_SITE = ./package/intree/src
+INTREE_SITE = ./package/intree/
 INTREE_SITE_METHOD = local
+MAKEOPT = -C $(@D)/src CC="$(TARGET_CC)" LD="$(TARGET_LD)"
 
 define COMPILE_EPOLL
-$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) epoll
+$(MAKE) $(MAKEOPT) epoll
 endef
 
 define COMPILE_HELLO
-$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) hello
+$(MAKE) $(MAKEOPT) hello
 endef
 
 define INSTALL_EPOLL
-$(INSTALL) -D -m 0755 $(@D)/../dist/epoll $(TARGET_DIR)/usr/bin
+$(INSTALL) -D -m 0755 $(@D)/dist/epoll $(TARGET_DIR)/usr/bin
 endef
 
 define INSTALL_HELLO
-$(INSTALL) -D -m 0755 $(@D)/../dist/hello $(TARGET_DIR)/usr/bin
+$(INSTALL) -D -m 0755 $(@D)/dist/hello $(TARGET_DIR)/usr/bin
 endef
 
 define INTREE_BUILD_CMDS
